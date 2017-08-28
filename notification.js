1b2d1c9809fc8bcb4f8420864b8b5afb06d129b6
Andrey Belousov <abelouso@gmail.com>
2017-08-28 21:28:35 +0300

    файрбейс

---
+++ notification.js
@@ -0,0 +1,51 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDnU949ApofKmeD_P2bv_AE5bpRIdz1pkg",
+    authDomain: "shtoringithubpushes.firebaseapp.com",
+    databaseURL: "https://shtoringithubpushes.firebaseio.com",
+    projectId: "shtoringithubpushes",
+    storageBucket: "shtoringithubpushes.appspot.com",
+    messagingSenderId: "279155818136"
+  };
+
+firebase.initializeApp(config);
+var messaging = firebase.messaging();
+
+// On load register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/firebase-messaging-sw.js').then((registration) => {
+      // Successfully registers service worker
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      messaging.useServiceWorker(registration);
+    })
+    .then(() => {
+      // Requests user browser permission
+      return messaging.requestPermission();
+    })
+    .then(() => {
+      // Gets token
+      return messaging.getToken();
+    })
+    .then((token) => {
+      console.log('Token that is not sending anywhere', token);
+      // Simple ajax call to send user token to server for saving
+      // $.ajax({
+      //   type: 'POST',
+      //   url: '/api/setToken',
+      //   dataType: 'json',
+      //   data: JSON.stringify({token: token}),
+      //   contentType: 'application/json',
+      //   success: (data) => {
+      //     console.log('Success ', data);
+      //   },
+      //   error: (err) => {
+      //     console.log('Error ', err);
+      //   }
+      // })
+    })
+    .catch((err) => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+  }
