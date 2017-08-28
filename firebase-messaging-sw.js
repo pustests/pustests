752aa2006964198e5b79dad9d6c2f35fbbe5e4f3
Andrey Belousov <abelouso@gmail.com>
2017-08-28 07:29:40 +0300

    onesignal

--- firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: '279155818136'
-});
-
-const messaging = firebase.messaging();
